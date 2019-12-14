58c5892cfff21349343ca643b4a5427441cd7cf7
Will Pitt <will@carsnip.com>
2019-12-14 00:45:18 +0000

    day 12

--- src/main/kotlin/y2019/d12/Day12.kt
+++ src/main/kotlin/y2019/d12/Day12.kt
@@ -1,7 +1,85 @@
 package y2019.d12
 
+import kotlin.math.abs
+
+fun main (){
+    Day12().inputMoons()
+}
+
 class Day12() {
-    fun main():String {
-        return ""
+    fun inputMoons() {
+        val xMovements = listOf(-19, -9, -4, 1).map{Movement(it, 0)}
+        val yMovements = listOf(-4, 8, 5, 9).map{Movement(it, 0)}
+        val zMovements = listOf(2, -16, -11, -13).map{Movement(it, 0)}
+        println (partA(xMovements, yMovements, zMovements))
+        println (partB(xMovements, yMovements, zMovements))
+    }
+
+    fun partB(xMovements: List<Movement>, yMovements: List<Movement>, zMovements: List<Movement>): Long {
+        var x = findLoop(xMovements)
+        var y = findLoop(yMovements)
+        var z = findLoop(zMovements)
+
+        var hcf = hcf(x, hcf(y, z))
+
+        return x.toLong()*y.toLong()*z.toLong()/hcf/hcf/hcf
+    }
+
+    fun hcf(a: Int, b: Int): Int {
+        var cd = if (abs(a) > abs(b)) Pair(abs(a), abs(b)) else Pair (abs(b), abs(a))
+        while( cd.second != 0) {
+            cd = Pair(cd.second, cd.first%cd.second)
+        }
+        return cd.first
+    }
+
+    fun findLoop(movements: List<Movement>): Int {
+        var newMoves = (0..3).map{calcNewMovements(movements[it], listWithout(movements, it))}
+        var index: Int = 1;
+        while(hash(newMoves) != hash(movements)) {
+            index++
+            newMoves = (0..3).map{calcNewMovements(newMoves[it], listWithout(newMoves, it))}
+        }
+        return index
+    }
+
+    fun hash(movements: List<Movement>): String {
+        return movements.map{it.pos.toString()+":"+it.speed}.joinToString()
+    }
+
+
+    fun partA(xMovements: List<Movement>, yMovements: List<Movement>, zMovements: List<Movement>): Int {
+        var newX = xMovements
+        var newY = yMovements
+        var newZ = zMovements
+        (0..999).forEach{
+            newX = (0..3).map{calcNewMovements(newX[it], listWithout(newX, it))}
+            newY = (0..3).map{calcNewMovements(newY[it], listWithout(newY, it))}
+            newZ = (0..3).map{calcNewMovements(newZ[it], listWithout(newZ, it))}
+        }
+        return calcEnergy(listOf(newX, newY, newZ))
+    }
+
+    fun calcEnergy(movements: List<List<Movement>>): Int {
+        return (0..3).map{ i ->  (0..2).map{abs(movements[it][i].pos)}.sum()*(0..2).map{ abs(movements[it][i].speed) }.sum()}.sum()
+    }
+
+    fun calcNewMovements(movement: Movement, otherMovements: List<Movement>): Movement {
+        val change: Int = otherMovements.map{ if (it.pos > movement.pos) { 1 } else if (it.pos == movement.pos) { 0 } else { -1 } }.sum()
+        return Movement(movement.pos + movement.speed + change, movement.speed + change)
+    }
+
+    fun <T> listWithout(list: List<T>, x: Int): List<T> {
+        return list.take(x) + list.drop(x+1)
+    }
+}
+
+data class Movement(var posA: Int, var speedA: Int) {
+    var pos: Int
+    var speed: Int
+
+    init{
+        this.pos = posA
+        this.speed = speedA
     }
 }
--- src/test/kotlin/y2019/d12/Day12Test.kt
+++ src/test/kotlin/y2019/d12/Day12Test.kt
@@ -7,8 +7,61 @@ class Day12Test{
 
     @test
     fun test() {
-        val result: String = Day12().main()
-
-        assertEquals(result, "")
+        val xMovements = listOf(Movement(-1, 0), Movement(2, 0), Movement(4, 0), Movement(3, 0))
+        assertEquals(Movement(2,3), Day12().calcNewMovements(xMovements[0], Day12().listWithout(xMovements, 0)))
+        assertEquals(Movement(3,1), Day12().calcNewMovements(xMovements[1], Day12().listWithout(xMovements, 1)))
+        assertEquals(Movement(1,-3), Day12().calcNewMovements(xMovements[2], Day12().listWithout(xMovements, 2)))
+        assertEquals(Movement(2,-1), Day12().calcNewMovements(xMovements[3], Day12().listWithout(xMovements, 3)))
     }
+
+    @test
+    fun testListWithout() {
+        val list = listOf(4,5,6,7)
+        assertEquals(listOf(5,6,7), Day12().listWithout(list, 0))
+        assertEquals(listOf(4,6,7), Day12().listWithout(list, 1))
+        assertEquals(listOf(4,5,7), Day12().listWithout(list, 2))
+        assertEquals(listOf(4,5,6), Day12().listWithout(list, 3))
+    }
+
+    @test
+    fun testCalcEnergy() {
+        val xMovements = listOf(Movement(2, -3), Movement(1, -1), Movement(3, 3), Movement(2, 1))
+        val yMovements = listOf(Movement(1, -2), Movement(-8, 1), Movement(-6, 2), Movement(0, -1))
+        val zMovements = listOf(Movement(-3, 1), Movement(0, 3), Movement(1, -3), Movement(4, -1))
+
+        assertEquals(179, Day12().calcEnergy(listOf(xMovements, yMovements, zMovements)))
+    }
+
+    @test
+    fun testPartA() {
+        val xMovements = listOf(-19, -9, -4, 1).map{Movement(it, 0)}
+        val yMovements = listOf(-4, 8, 5, 9).map{Movement(it, 0)}
+        val zMovements = listOf(2, -16, -11, -13).map{Movement(it, 0)}
+
+        assertEquals(8287, Day12().partA(xMovements, yMovements, zMovements))
+    }
+
+    @test
+    fun testFindLoop() {
+        val xMovements = listOf(-1, 2, 4, 3).map{Movement(it, 0)}
+        val yMovements = listOf(0, -10, -8, 5).map{Movement(it, 0)}
+        val zMovements = listOf(2, -7, 8, -1).map{Movement(it, 0)}
+        assertEquals(18, Day12().findLoop(xMovements))
+        assertEquals(28, Day12().findLoop(yMovements))
+        assertEquals(44, Day12().findLoop(zMovements))
+    }
+
+    @test
+    fun testPartB() {
+        val xMovements = listOf(-1, 2, 4, 3).map{Movement(it, 0)}
+        val yMovements = listOf(0, -10, -8, 5).map{Movement(it, 0)}
+        val zMovements = listOf(2, -7, 8, -1).map{Movement(it, 0)}
+        assertEquals(2772, Day12().partB(xMovements, yMovements, zMovements))
+    }
+
+    @test
+    fun testHcf() {
+        assertEquals(3, Day12().hcf(Day12().hcf(3,6),9))
+    }
+
 }
