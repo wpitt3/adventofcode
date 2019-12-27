47afa99330df21c0d071332bee8a819e693fed84
Will Pitt <will@carsnip.com>
2019-12-27 12:25:36 +0000

    Day20

--- src/main/kotlin/y2019/d20/Day20.kt
+++ src/main/kotlin/y2019/d20/Day20.kt
@@ -1,18 +1,120 @@
 package y2019.d20
 
-fun main():String {
-    return ""
+import java.io.File
+
+fun main() {
+    val lines = File("src/main/resources/y2019d20.txt").readLines()
+    val portals = Day20().linesToPortals(lines)
+    val map = Day20().lineToInternalGrid(lines, portals)
+    println (Day20().wanderFlat(map, portals))
+    println (Day20().wander(map, portals))
 }
+
 class Day20() {
+    fun wander(map: List<List<Tile>>, portals: List<Portal>): Int {
+        val startPosPair = getMatchingPortal(portals, "AA")
+        val startPos = Triple(startPosPair.first, startPosPair.second, 0)
+        val threeDMap = cleanCloneMap(map, 200)
+        threeDMap[startPos.third][startPos.second][startPos.first].isVisited = true
 
-    fun wander(portals: List<Portal>) {
+        var counter = 0
+        var cursors = mutableListOf(startPos)
+        while(cursors.all{!(threeDMap[it.third][it.second][it.first].isEnd && it.third == 0) }) {
+            cursors = cursors.map { findNextLocations(threeDMap, portals, it) }.flatten().toMutableList()
+            cursors.forEach{ threeDMap[it.third][it.second][it.first].isVisited = true }
+            counter++
+        }
+        return counter
+    }
 
+    fun findNextLocations(map: List<List<List<Tile>>>, portals: List<Portal>, currentPos: Triple<Int, Int, Int>): MutableList<Triple<Int, Int, Int>> {
+        var nextPositions: MutableList<Triple<Int, Int, Int>> = mutableListOf()
+        if ( isLocationAvailable(map[currentPos.third], currentPos.first, currentPos.second-1)) {
+            nextPositions.add(Triple(currentPos.first, currentPos.second-1, currentPos.third))
+        }
+        if ( isLocationAvailable(map[currentPos.third], currentPos.first, currentPos.second+1)) {
+            nextPositions.add(Triple(currentPos.first, currentPos.second+1, currentPos.third))
+        }
+        if ( isLocationAvailable(map[currentPos.third], currentPos.first-1, currentPos.second)) {
+            nextPositions.add(Triple(currentPos.first-1, currentPos.second, currentPos.third))
+        }
+        if ( isLocationAvailable(map[currentPos.third], currentPos.first+1, currentPos.second)) {
+            nextPositions.add(Triple(currentPos.first+1, currentPos.second, currentPos.third))
+        }
+        if ( map[currentPos.third][currentPos.second][currentPos.first].isPortal()
+            && !map[currentPos.third][currentPos.second][currentPos.first].isStart
+            && !map[currentPos.third][currentPos.second][currentPos.first].isEnd) {
+            val portal = map[currentPos.third][currentPos.second][currentPos.first].portal !!
+            val otherSide = portals.filter{it.name == portal.name && it.outside != portal.outside}.first() !!
+            val modifier = if (portal.outside) -1 else 1
+            if (currentPos.third + modifier >= 0 && !map[currentPos.third + modifier][otherSide.y][otherSide.x].isVisited) {
+                nextPositions.add(Triple(otherSide.x, otherSide.y, currentPos.third + modifier))
+            }
+        }
+        return nextPositions
+    }
+
+    fun cleanCloneMap(map: List<List<Tile>>, depth: Int): List<List<List<Tile>>> {
+        return (1..(depth)).map{ map.map{a -> a.map{Tile(it.portal, it.label)}}}
+    }
+
+    fun wanderFlat(map: List<List<Tile>>, portals: List<Portal>): Int {
+        val startPos = getMatchingPortal(portals, "AA")
+
+        map[startPos.second][startPos.first].isVisited = true
+
+        var counter = 0
+        var cursors = mutableListOf(startPos)
+        while(cursors.all{!map[it.second][it.first].isEnd}) {
+            cursors = cursors.map { findNextLocationsFlat(map, portals, it) }.flatten().toMutableList()
+
+            cursors.forEach{ map[it.second][it.first].isVisited = true }
+
+            counter++
+        }
+
+        return counter
+    }
+
+    fun findNextLocationsFlat(map: List<List<Tile>>, portals: List<Portal>, currentPos: Pair<Int, Int>): MutableList<Pair<Int, Int>> {
+        var nextPositions: MutableList<Pair<Int, Int>> = mutableListOf()
+        if ( isLocationAvailable(map, currentPos.first, currentPos.second-1)) {
+            nextPositions.add(Pair(currentPos.first, currentPos.second-1))
+        }
+        if ( isLocationAvailable(map, currentPos.first, currentPos.second+1)) {
+            nextPositions.add(Pair(currentPos.first, currentPos.second+1))
+        }
+        if ( isLocationAvailable(map, currentPos.first-1, currentPos.second)) {
+            nextPositions.add(Pair(currentPos.first-1, currentPos.second))
+        }
+        if ( isLocationAvailable(map, currentPos.first+1, currentPos.second)) {
+            nextPositions.add(Pair(currentPos.first+1, currentPos.second))
+        }
+        if ( map[currentPos.second][currentPos.first].isPortal() && !map[currentPos.second][currentPos.first].isStart && !map[currentPos.second][currentPos.first].isEnd) {
+            val portal = map[currentPos.second][currentPos.first].portal !!
+            val otherSide = portals.filter{it.name == portal.name && it.outside != portal.outside}.first() !!
+            nextPositions.add(Pair(otherSide.x, otherSide.y))
+        }
+        return nextPositions
+    }
+
+    fun isLocationAvailable(map: List<List<Tile>>, x: Int, y: Int): Boolean {
+        return !map[y][x].isWall && !map[y][x].isVisited
+    }
+
+    fun getMatchingPortal(portals: List<Portal>, name :String): Pair<Int, Int> {
+        return portals.filter{it.name == name}.map{Pair(it.x, it.y)}.first() !!
     }
 
     fun lineToInternalGrid(lines: List<String>, portals: List<Portal>): List<List<Tile>> {
         val gridSquares: List<List<String>> = lines.map{it.split("").filter{!it.isEmpty()}}
 
-        return listOf(listOf())
+        return (0..(gridSquares.size - 1)).map { y ->
+            (0..(gridSquares[0].size-1)).map { x ->
+                val portal: Portal? = portals.filter{it.x == x && it.y == y}.firstOrNull()
+                Tile(portal, gridSquares[y][x])
+            }
+        }
     }
 
     fun linesToPortals(lines: List<String>): List<Portal> {
@@ -26,39 +128,58 @@ class Day20() {
         val xMaxInterIndex = xInternalRange[xInternalRange.size-1]
         val yMaxInterIndex = yInternalRange[yInternalRange.size-1]
 
-        portals.addAll(findPortalsX(gridSquares, yInternalRange[0]-3, yInternalRange[0], xInternalRange, false))
-        portals.addAll(findPortalsX(gridSquares, yMaxInterIndex-1, yMaxInterIndex-1, xInternalRange, false))
+        portals.addAll(findPortalsX(gridSquares, yInternalRange[0]-1, yInternalRange[0], xInternalRange, false))
+        portals.addAll(findPortalsX(gridSquares, yMaxInterIndex+1, yMaxInterIndex-1, xInternalRange, false))
 
-        portals.addAll(findPortalsX(gridSquares, 0, 0, (2..gridWidth).toList(), true))
-        portals.addAll(findPortalsX(gridSquares, gridHeight-4, gridHeight-1, (2..gridWidth).toList(), true))
+        portals.addAll(findPortalsX(gridSquares, 2, 0, (2..gridWidth).toList(), true))
+        portals.addAll(findPortalsX(gridSquares, gridHeight-2, gridHeight-1, (2..gridWidth).toList(), true))
 
-        portals.addAll(findPortalsY(gridSquares, xInternalRange[0]-3, xInternalRange[0], yInternalRange, false))
-        portals.addAll(findPortalsY(gridSquares, xMaxInterIndex-1, xMaxInterIndex-1, yInternalRange, false))
+        portals.addAll(findPortalsY(gridSquares, xInternalRange[0]-1, xInternalRange[0], yInternalRange, false))
+        portals.addAll(findPortalsY(gridSquares, xMaxInterIndex+1, xMaxInterIndex-1, yInternalRange, false))
 
-        portals.addAll(findPortalsY(gridSquares, 0, 0, (2..gridHeight).toList(), true))
-        portals.addAll(findPortalsY(gridSquares, gridWidth-4, gridWidth-1, (2..gridHeight).toList(), true))
+        portals.addAll(findPortalsY(gridSquares, 2, 0, (2..gridHeight).toList(), true))
+        portals.addAll(findPortalsY(gridSquares, gridWidth-2, gridWidth-1, (2..gridHeight).toList(), true))
         return portals
     }
 
     fun findPortalsX(gridSquares: List<List<String>>, yIndex: Int, yLabelIndex: Int, xInternalRange: List<Int>, outside: Boolean): List<Portal> {
         return xInternalRange.filter{!gridSquares[yLabelIndex][it].isBlank() && !gridSquares[yLabelIndex+1][it].isBlank()}
-            .map {Portal(it-2, yIndex, gridSquares[yLabelIndex][it]+gridSquares[yLabelIndex+1][it], outside)}
+            .map {Portal(it, yIndex, gridSquares[yLabelIndex][it]+gridSquares[yLabelIndex+1][it], outside)}
     }
 
     fun findPortalsY(gridSquares: List<List<String>>, xIndex: Int, xLabelIndex: Int, yInternalRange: List<Int>, outside: Boolean): List<Portal> {
         return yInternalRange.filter{!gridSquares[it][xLabelIndex].isBlank() && !gridSquares[it][xLabelIndex+1].isBlank()}
-            .map {Portal(xIndex, it-2, gridSquares[it][xLabelIndex]+gridSquares[it][xLabelIndex+1], outside)}
+            .map {Portal(xIndex, it, gridSquares[it][xLabelIndex]+gridSquares[it][xLabelIndex+1], outside)}
     }
 }
 
-class Tile(val portal: Portal?, label: String) {
+data class Tile(val portal: Portal?, val label: String) {
     val isStart: Boolean = portal?.name == "AA"
     val isEnd: Boolean = portal?.name == "ZZ"
-    val isWall: Boolean = label == "#"
+    val isWall: Boolean = label != "."
     var isVisited: Boolean = false
 
     fun isPortal(): Boolean {
         return portal != null
+    }
+
+    override fun toString(): String {
+        if (isWall) {
+            return "."
+        }
+        if (isStart) {
+            return "s"
+        }
+        if (isEnd) {
+            return "e"
+        }
+        if (isVisited) {
+            return "v"
+        }
+        if (isPortal()) {
+            return "p"
+        }
+        return " "
     }
 }
 
--- src/test/kotlin/y2019/d20/Day20Test.kt
+++ src/test/kotlin/y2019/d20/Day20Test.kt
@@ -12,20 +12,21 @@ class Day20Test{
         val lines = File("src/test/resources/y2019d20a.txt").readLines()
 
         val result = Day20().linesToPortals(lines)
-        assertTrue(result.contains(Portal(7, 0, "AA", true)))
-        assertTrue(result.contains(Portal(11, 14, "ZZ", true)))
-        assertTrue(result.contains(Portal(0, 6, "BC", true)))
-        assertTrue(result.contains(Portal(16, 5, "HI", true)))
-        assertTrue(result.contains(Portal(7, 4, "BC", false)))
-        assertTrue(result.contains(Portal(9, 10, "FG", false)))
-        assertTrue(result.contains(Portal(4, 8, "DE", false)))
-        assertTrue(result.contains(Portal(12, 6, "HI", false)))
+        assertTrue(result.contains(Portal(9, 2, "AA", true)))
+        assertTrue(result.contains(Portal(13, 16, "ZZ", true)))
+        assertTrue(result.contains(Portal(2, 8, "BC", true)))
+        assertTrue(result.contains(Portal(18, 7, "HI", true)))
+        assertTrue(result.contains(Portal(9, 6, "BC", false)))
+        assertTrue(result.contains(Portal(11, 12, "FG", false)))
+        assertTrue(result.contains(Portal(6, 10, "DE", false)))
+        assertTrue(result.contains(Portal(14, 8, "HI", false)))
         assertEquals(10, result.size)
     }
 
     @test
     fun testTile() {
         assertEquals(Portal(0,0,"", true), Tile(Portal(0,0,"", true), ".").portal)
+
         assertEquals(true, Tile(Portal(0,0,"", true), ".").isPortal())
         assertEquals(false, Tile(null, ".").isPortal())
         assertEquals(false, Tile(Portal(0,0,"", true), ".").isStart)
@@ -35,6 +36,7 @@ class Day20Test{
         assertEquals(false, Tile(Portal(0,0,"", true), ".").isVisited)
         assertEquals(true, Tile(null, "#").isWall)
         assertEquals(false, Tile(null, ".").isWall)
+        assertEquals(true, Tile(null, " ").isWall)
     }
 
     @test
@@ -44,8 +46,114 @@ class Day20Test{
         val portals = Day20().linesToPortals(lines)
         val result = Day20().lineToInternalGrid(lines, portals)
 
-        println(result)
+        assertEquals(true, result[2][9].isStart)
+        assertEquals(false, result[2][9].isWall)
+        assertEquals(false, result[1][9].isStart)
+        assertEquals(true, result[1][9].isWall)
 
-        assertEquals(false, Tile(null, ".").isWall)
+        assertEquals(true, result[16][13].isEnd)
+        assertEquals(false, result[16][13].isWall)
+
+        assertEquals("BC", result[8][2].portal?.name)
+        assertEquals(true, result[8][2].portal?.outside)
+        assertEquals("BC", result[6][9].portal?.name)
+        assertEquals(false, result[6][9].portal?.outside)
+    }
+
+    @test
+    fun testGetMatchingPortal() {
+        val lines = File("src/test/resources/y2019d20a.txt").readLines()
+
+        val portals = Day20().linesToPortals(lines)
+
+        assertEquals(Pair(9, 2), Day20().getMatchingPortal(portals, "AA"))
+        assertEquals(Pair(13, 16), Day20().getMatchingPortal(portals, "ZZ"))
+    }
+
+    @test
+    fun testFindNextLocationFlat() {
+        val lines = File("src/test/resources/y2019d20a.txt").readLines()
+
+        val portals = Day20().linesToPortals(lines)
+        val map = Day20().lineToInternalGrid(lines, portals)
+        map[2][9].isVisited = true
+
+        assertEquals(mutableListOf(Pair(9, 3)), Day20().findNextLocationsFlat(map, portals, Pair(9, 2)))
+        assertEquals(mutableListOf(Pair(9, 4), Pair(10, 3)), Day20().findNextLocationsFlat(map, portals, Pair(9, 3)))
+        assertEquals(mutableListOf(Pair(9, 5), Pair(2, 8)), Day20().findNextLocationsFlat(map, portals, Pair(9, 6)))
+    }
+
+    @test
+    fun testWanderA() {
+        val lines = File("src/test/resources/y2019d20a.txt").readLines()
+
+        val portals = Day20().linesToPortals(lines)
+        val map = Day20().lineToInternalGrid(lines, portals)
+
+        val result = Day20().wanderFlat(map, portals)
+
+        assertEquals(23, result)
+    }
+
+    @test
+    fun testWanderB() {
+        val lines = File("src/test/resources/y2019d20b.txt").readLines()
+
+        val portals = Day20().linesToPortals(lines)
+        val map = Day20().lineToInternalGrid(lines, portals)
+        val result = Day20().wanderFlat(map, portals)
+
+        assertEquals(58, result)
+    }
+
+    @test
+    fun testWanderANotFlat() {
+        val lines = File("src/test/resources/y2019d20a.txt").readLines()
+
+        val portals = Day20().linesToPortals(lines)
+        val map = Day20().lineToInternalGrid(lines, portals)
+
+        val result = Day20().wander(map, portals)
+
+        assertEquals(26, result)
+    }
+
+    @test
+    fun testWanderCNotFlat() {
+        val lines = File("src/test/resources/y2019d20c.txt").readLines()
+
+        val portals = Day20().linesToPortals(lines)
+        val map = Day20().lineToInternalGrid(lines, portals)
+
+        val result = Day20().wander(map, portals)
+
+        assertEquals(396, result)
+    }
+
+    @test
+    fun testFindNextLocation() {
+        val lines = File("src/test/resources/y2019d20a.txt").readLines()
+
+        val portals = Day20().linesToPortals(lines)
+        val map = Day20().lineToInternalGrid(lines, portals)
+        val threedMap = Day20().cleanCloneMap(map, 5)
+        threedMap[0][2][9].isVisited = true
+
+        assertEquals(mutableListOf(Triple(9, 3, 0)), Day20().findNextLocations(threedMap, portals, Triple(9, 2, 0)))
+        assertEquals(mutableListOf(Triple(9, 4, 0), Triple(10, 3, 0)), Day20().findNextLocations(threedMap, portals, Triple(9, 3, 0)))
+        assertEquals(mutableListOf(Triple(9, 5, 0), Triple(2, 8, 1)), Day20().findNextLocations(threedMap, portals, Triple(9, 6, 0)))
+    }
+
+    @test
+    fun testCleanCloneMap() {
+        val lines = File("src/test/resources/y2019d20a.txt").readLines()
+
+        val portals = Day20().linesToPortals(lines)
+        val map = Day20().lineToInternalGrid(lines, portals)
+
+        val result = Day20().cleanCloneMap(map, 5)
+        result[0][2][9].isVisited = true
+
+        assertEquals(false, result[1][2][9].isVisited)
     }
 }
