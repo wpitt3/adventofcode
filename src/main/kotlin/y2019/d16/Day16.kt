db62d33b52a045d54200e25ca7a3a07e188ca600
Will Pitt <will@carsnip.com>
2020-01-20 22:41:43 +0000

    day16

--- src/main/kotlin/y2019/d16/Day16.kt
+++ src/main/kotlin/y2019/d16/Day16.kt
@@ -1,5 +1,39 @@
+package y2019.d16
+
+import java.io.File
+import kotlin.math.abs
+
+fun main() {
+    val lines: List<String> = File("src/main/resources/y2019d16.txt").readLines()
+    var numbers = lines[0].split("").filter{it != ""}.map{ it.toInt()}
+    println(System.currentTimeMillis())
+    repeat(100){
+        numbers = Day16().runPattern(numbers)
+    }
+    println(numbers.take(8).joinToString(""))
+    println(System.currentTimeMillis())
+}
+
 class Day16() {
-    fun main():String {
-        return ""
+
+    fun runPattern(input: List<Int>): List<Int> {
+        return (0..(input.size-1)).map { y ->
+            val z:Int = (0..(input.size-1)).map { x ->
+                findMultiplierForIndex(y, x) * input[x]
+            }.sum()
+            abs(z) % 10
+        }
+    }
+
+    fun findMultiplierForIndex(row: Int, column: Int): Int {
+        val step = (row + 1) * 4
+        val patternIndex = ((column + 1) % step) / (row + 1)
+        if (patternIndex == 1) {
+            return 1
+        } else if (patternIndex == 3) {
+            return -1
+        } else {
+            return 0
+        }
     }
 }
--- src/test/kotlin/y2019/d16/Day16Test.kt
+++ src/test/kotlin/y2019/d16/Day16Test.kt
@@ -1,13 +1,38 @@
-import Day16
+package y2019.d15
+
+import y2019.d16.Day16
 import kotlin.test.assertEquals
 import org.junit.Test as test
 
 class Day16Test{
 
     @test
-    fun test() {
-        val result: String = Day16().main()
+    fun row0FollowsPatter010minus1OffetByOne() {
+        assertEquals(1, Day16().findMultiplierForIndex(0, 0))
+        assertEquals(0, Day16().findMultiplierForIndex(0, 1))
+        assertEquals(-1, Day16().findMultiplierForIndex(0, 2))
+        assertEquals(0, Day16().findMultiplierForIndex(0, 3))
+    }
 
-        assertEquals(result, "")
+    @test
+    fun row1FollowsPatternButMultipliedInPatchesOf2() {
+        assertEquals(0, Day16().findMultiplierForIndex(1, 0))
+        assertEquals(1, Day16().findMultiplierForIndex(1, 1))
+        assertEquals(1, Day16().findMultiplierForIndex(1, 2))
+        assertEquals(0, Day16().findMultiplierForIndex(1, 3))
+        assertEquals(0, Day16().findMultiplierForIndex(1, 4))
+        assertEquals(-1, Day16().findMultiplierForIndex(1, 5))
+    }
+
+    @test
+    fun row7FollowsPattern() {
+        assertEquals(0, Day16().findMultiplierForIndex(8, 7))
+        assertEquals(1, Day16().findMultiplierForIndex(8, 8))
+    }
+
+    @test
+    fun convertInput() {
+        assertEquals(listOf(4,8,2,2,6,1,5,8), Day16().runPattern(listOf(1,2,3,4,5,6,7,8)))
+        assertEquals(listOf(3,4,0,4,0,4,3,8), Day16().runPattern(listOf(4,8,2,2,6,1,5,8)))
     }
 }
