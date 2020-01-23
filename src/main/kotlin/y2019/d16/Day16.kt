4eb0a147ed14c10a432f2922afc4a281c8fb86b1
Will Pitt <william.pitt@treatwell.com>
2020-01-23 18:08:54 +0000

    not much

--- src/main/kotlin/y2019/d16/Day16.kt
+++ src/main/kotlin/y2019/d16/Day16.kt
@@ -6,34 +6,38 @@ import kotlin.math.abs
 fun main() {
     val lines: List<String> = File("src/main/resources/y2019d16.txt").readLines()
     var numbers = lines[0].split("").filter{it != ""}.map{ it.toInt()}
-    println(System.currentTimeMillis())
+    val start: Long = System.currentTimeMillis()
+    println(runPattern100Times(numbers).take(8).joinToString(""))
+    println(82435530)
+    println(System.currentTimeMillis() - start)
+}
+
+fun runPattern100Times(input: List<Int>): List<Int> {
+    var numbers = input
     repeat(100){
-        numbers = Day16().runPattern(numbers)
+        numbers = runPattern(numbers)
     }
-    println(numbers.take(8).joinToString(""))
-    println(System.currentTimeMillis())
+    return numbers
 }
 
-class Day16() {
-
-    fun runPattern(input: List<Int>): List<Int> {
-        return (0..(input.size-1)).map { y ->
-            val z:Int = (0..(input.size-1)).map { x ->
-                findMultiplierForIndex(y, x) * input[x]
-            }.sum()
-            abs(z) % 10
-        }
-    }
-
-    fun findMultiplierForIndex(row: Int, column: Int): Int {
-        val step = (row + 1) * 4
-        val patternIndex = ((column + 1) % step) / (row + 1)
-        if (patternIndex == 1) {
-            return 1
-        } else if (patternIndex == 3) {
-            return -1
-        } else {
-            return 0
-        }
+fun runPattern(input: List<Int>): List<Int> {
+    return (0..(input.size-1)).map { y ->
+        val z:Int = (0..(input.size-1)).map { x ->
+            findMultiplierForIndex(y, x) * input[x]
+        }.sum()
+        abs(z) % 10
     }
 }
+
+fun findMultiplierForIndex(row: Int, column: Int): Int {
+    val step = (row + 1) * 4
+    val patternIndex = ((column + 1) % step) / (row + 1)
+    if (patternIndex == 1) {
+        return 1
+    } else if (patternIndex == 3) {
+        return -1
+    } else {
+        return 0
+    }
+}
+
