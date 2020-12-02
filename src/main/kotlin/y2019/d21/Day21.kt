a0739681bd68f33a2e96b8aa692b76a3b6aab0eb
will <william.pitt@treatwell.com>
2020-12-02 22:48:39 +0000

    day21

--- src/main/kotlin/y2019/d21/Day21.kt
+++ src/main/kotlin/y2019/d21/Day21.kt
@@ -1,5 +1,67 @@
-class Day21() {
-    fun main():String {
-        return ""
+import y2019.IntcodeProcessor
+import java.io.File
+
+fun main() {
+    val lines: List<String> = File("src/main/resources/y2019d21.txt").readLines()
+    val instructions: List<Long> = lines[0].split(",").map({x -> (x).toLong()}).toList()
+    day21a(instructions)
+    day21b(instructions)
+}
+
+fun day21a(instructions: List<Long>) {
+    val robot = Robot(instructions, listOf(
+            "NOT A J",
+            "NOT B T",
+            "OR J T",
+            "NOT C J",
+            "OR T J",
+            "AND D J",
+            "WALK"
+    ))
+    robot.run()
+    println(robot.result())
+}
+
+fun day21b(instructions: List<Long>) {
+    val robot = Robot(instructions, listOf(
+            "NOT A J",
+            "NOT B T",
+            "OR J T",
+            "NOT C J",
+            "OR T J",
+
+            "NOT J T",
+            "OR E T",
+            "OR H T",
+            "AND D T",
+            "AND T J",
+            "RUN"
+    ))
+    robot.run()
+    println(robot.result())
+}
+
+class Robot(val intCodeInstructions: List<Long>, springScriptInstructions: List<String>) {
+    var processor = IntcodeProcessor(intCodeInstructions.toMutableList())
+    var output = mutableListOf<Int>()
+    var inputs = mutableListOf<Long>()
+    var inputIndex = 0
+
+    init {
+        springScriptInstructions.forEach{line ->
+            inputs.addAll(line.toCharArray().map{it.toInt().toLong()})
+            inputs.add(10)
+        }
+    }
+
+    fun run() {
+        processor.callOutput = { it ->  output.add(it.toInt())}
+        processor.callInput = { inputs[inputIndex++] }
+        processor.reset(intCodeInstructions.toMutableList())
+        processor.run()
+    }
+
+    fun result(): String {
+        return output.map{if (it < 1000000) it.toInt().toChar().toString() else it.toString()}.joinToString()
     }
 }
--- src/test/kotlin/y2019/d21/Day21Test.kt
+++ src/test/kotlin/y2019/d21/Day21Test.kt
@@ -1,4 +1,4 @@
-import Day21
+
 import kotlin.test.assertEquals
 import org.junit.Test as test
 
@@ -6,8 +6,63 @@ class Day21Test{
 
     @test
     fun test() {
-        val result: String = Day21().main()
+        val machineCode = listOf(
+                "NOT A J",
+                "NOT B T",
+                "OR J T",
+                "NOT C J",
+                "OR T J",
+                "NOT J T",
+                "OR E T",
+                "OR H T",
+                "AND D T",
+                "AND T J",
+                "RUN"
+        )
 
-        assertEquals(result, "")
+        assertEquals(runMachineCode(machineCode, "...######"), true)
+        assertEquals(runMachineCode(machineCode, "##.#.##.#"), false)
+    }
+
+
+    fun runMachineCode(machineCode: List<String>, scenario: String): Boolean {
+        var J = false
+        var T = false
+        val grid = scenario.map{it.toString() == "#"}
+
+        machineCode.forEach{
+            if (it.startsWith("NOT")) {
+                val x = it[4].toInt() - 65
+                val y = it[6].toInt() - 65
+                val xVal = if (x>8) (if (x == 9) J else T) else grid[x]
+                if (y==9) {
+                    J = !xVal
+                } else {
+                    T = !xVal
+                }
+            } else if(it.startsWith("AND")) {
+                val x = it[4].toInt() - 65
+                val y = it[6].toInt() - 65
+                val xVal = if (x>8) (if (x == 9) J else T) else grid[x]
+                if (y==9) {
+                    J = J && xVal
+                } else {
+                    T = T && xVal
+                }
+            } else if(it.startsWith("OR")) {
+                val x = it[3].toInt() - 65
+                val y = it[5].toInt() - 65
+                val xVal = if (x>8) (if (x == 9) J else T) else grid[x]
+                if (y==9) {
+                    J = J || xVal
+                } else {
+                    T = T || xVal
+                }
+            }
+        }
+
+        println("T =" + T )
+        println("J =" + J )
+        return J
     }
 }
