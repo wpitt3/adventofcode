81f6ce55198721d2d8b4ca4916ba7f29ebe75368
will <william.pitt@treatwell.com>
2020-12-02 17:48:20 +0000

    day16 update

--- src/main/kotlin/y2019/d16/Day16.kt
+++ src/main/kotlin/y2019/d16/Day16.kt
@@ -2,42 +2,59 @@ package y2019.d16
 
 import java.io.File
 import kotlin.math.abs
+import kotlin.math.min
 
 fun main() {
     val lines: List<String> = File("src/main/resources/y2019d16.txt").readLines()
-    var numbers = lines[0].split("").filter{it != ""}.map{ it.toInt()}
-    val start: Long = System.currentTimeMillis()
+    var numbers = lines[0].split("").filter{it != ""}.map{ it.toLong()}
+    var longNumbers = (1..10000).map{ numbers }.flatten()
+
     println(runPattern100Times(numbers).take(8).joinToString(""))
-    println(82435530)
-    println(System.currentTimeMillis() - start)
+    val offset = numbers.take(7).joinToString("").toInt()
+    println(runPattern100Times(longNumbers).drop(offset).take(8).joinToString(""))
+
 }
 
-fun runPattern100Times(input: List<Int>): List<Int> {
+fun runPattern100Times(input: List<Long>): List<Long> {
+    val start: Long = System.currentTimeMillis()
     var numbers = input
     repeat(100){
         numbers = runPattern(numbers)
+        println(it.toString() + ": " + (System.currentTimeMillis() - start))
     }
     return numbers
 }
 
-fun runPattern(input: List<Int>): List<Int> {
-    return (0..(input.size-1)).map { y ->
-        val z:Int = (0..(input.size-1)).map { x ->
-            findMultiplierForIndex(y, x) * input[x]
-        }.sum()
-        abs(z) % 10
+fun runPattern(input: List<Long>): List<Long> {
+    val cumulativeTotal = calcCumulativeTotal(input)
+    val result = mutableListOf<Long>()
+    for (row in 0..(input.size-1)) {
+        val groupSize = row+1
+        var index = row
+        var total = 0L
+        var multiplier = 1
+        while ( index < input.size) {
+            total += multiplier * calcTotalForRange(cumulativeTotal, index, min(index+groupSize-1, input.size-1))
+            multiplier = multiplier * -1
+            index = index + groupSize * 2
+        }
+        result.add(abs(total)%10)
     }
+    return result
 }
 
-fun findMultiplierForIndex(row: Int, column: Int): Int {
-    val step = (row + 1) * 4
-    val patternIndex = ((column + 1) % step) / (row + 1)
-    if (patternIndex == 1) {
-        return 1
-    } else if (patternIndex == 3) {
-        return -1
+fun calcCumulativeTotal(input: List<Long>): List<Long> {
+    val cumulativeTotal = mutableListOf(input[0])
+    for (i in 1..(input.size-1)) {
+        cumulativeTotal.add((cumulativeTotal[i-1] + input[i]))
+    }
+    return cumulativeTotal
+}
+
+fun calcTotalForRange(cumulativeTotal: List<Long>, from: Int, to: Int): Long {
+    if  (from == 0) {
+        return cumulativeTotal[to]
     } else {
-        return 0
+        return cumulativeTotal[to] - cumulativeTotal[from-1]
     }
 }
-
--- src/test/kotlin/y2019/d16/Day16Test.kt
+++ src/test/kotlin/y2019/d16/Day16Test.kt
@@ -1,38 +1,17 @@
 package y2019.d15
 
-import y2019.d16.Day16
+import y2019.d16.runPattern
 import kotlin.test.assertEquals
 import org.junit.Test as test
 
 class Day16Test{
 
-    @test
-    fun row0FollowsPatter010minus1OffetByOne() {
-        assertEquals(1, Day16().findMultiplierForIndex(0, 0))
-        assertEquals(0, Day16().findMultiplierForIndex(0, 1))
-        assertEquals(-1, Day16().findMultiplierForIndex(0, 2))
-        assertEquals(0, Day16().findMultiplierForIndex(0, 3))
-    }
 
     @test
-    fun row1FollowsPatternButMultipliedInPatchesOf2() {
-        assertEquals(0, Day16().findMultiplierForIndex(1, 0))
-        assertEquals(1, Day16().findMultiplierForIndex(1, 1))
-        assertEquals(1, Day16().findMultiplierForIndex(1, 2))
-        assertEquals(0, Day16().findMultiplierForIndex(1, 3))
-        assertEquals(0, Day16().findMultiplierForIndex(1, 4))
-        assertEquals(-1, Day16().findMultiplierForIndex(1, 5))
-    }
-
-    @test
-    fun row7FollowsPattern() {
-        assertEquals(0, Day16().findMultiplierForIndex(8, 7))
-        assertEquals(1, Day16().findMultiplierForIndex(8, 8))
-    }
-
-    @test
-    fun convertInput() {
-        assertEquals(listOf(4,8,2,2,6,1,5,8), Day16().runPattern(listOf(1,2,3,4,5,6,7,8)))
-        assertEquals(listOf(3,4,0,4,0,4,3,8), Day16().runPattern(listOf(4,8,2,2,6,1,5,8)))
+    fun runPattern2() {
+        assertEquals(listOf<Long>(4,8,2,2,6,1,5,8), runPattern(listOf(1,2,3,4,5,6,7,8)))
+        assertEquals(listOf<Long>(3,4,0,4,0,4,3,8), runPattern(listOf(4,8,2,2,6,1,5,8)))
+        assertEquals(listOf<Long>(0,3,4,1,5,5,1,8), runPattern(listOf(3,4,0,4,0,4,3,8)))
+        assertEquals(listOf<Long>(0,1,0,2,9,4,9,8), runPattern(listOf(0,3,4,1,5,5,1,8)))
     }
 }
